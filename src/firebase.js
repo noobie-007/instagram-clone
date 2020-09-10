45abd552e30b6c2df39912e7615127923438bc83
piyush hambardikar <hambardikarp123@gmail.com>
2020-09-10 19:24:22 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyB9iJ-wwXJigVJq7gFbbGxcO2pMnhe2rIE",
-    authDomain: "instagram-clone-a2cd7.firebaseapp.com",
-    databaseURL: "https://instagram-clone-a2cd7.firebaseio.com",
-    projectId: "instagram-clone-a2cd7",
-    storageBucket: "instagram-clone-a2cd7.appspot.com",
-    messagingSenderId: "740819507138",
-    appId: "1:740819507138:web:36d52116a7cb7ba702e85b",
-    measurementId: "G-D72DK1J244"    
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""    
 }); 
 
 const db = firebaseApp.firestore();
